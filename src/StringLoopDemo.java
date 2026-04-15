feef466951240b3c77643b827fd60214847d50f2
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-15 16:08:17 +0400

    a string is declared as a sentence and printed using a loop

---
+++ src/StringLoopDemo.java
@@ -0,0 +1,13 @@
+public class StringLoopDemo {
+    public static void main(String[] args){
+        String str = " I am solving my tasks regularly  ";
+        System.out.println(str);
+
+        System.out.println(str.length());
+
+        for (int i=0; i<str.length();i++){
+            System.out.println(str);
+        }
+
+    }
+}
