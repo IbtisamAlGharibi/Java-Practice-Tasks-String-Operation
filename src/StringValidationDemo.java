1b1678a7f482126f0a834a76982d6c55fdb08459
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-15 15:58:28 +0400

    create a string and check if its starts and ends with specific letters

---
+++ src/StringValidationDemo.java
@@ -0,0 +1,10 @@
+public class StringValidationDemo {
+    public static void main(String[] args){
+        String str = "Ibtisam";
+        System.out.println(str);
+
+        System.out.println(str.startsWith("I"));
+        System.out.println(str.endsWith("a"));
+
+    }
+}
