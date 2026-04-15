02584cfcbbea56c264ef231a746d4ffbc9746f2a
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-15 16:13:54 +0400

    the string is declared as a sentence and check its length

---
+++ src/StringWhileDemo.java
@@ -0,0 +1,11 @@
+public class StringWhileDemo {
+    public static void main(String[] args){
+        String str = " I am solving my tasks regularly  ";
+        System.out.println(str);
+
+        System.out.println(str.length());
+        int count = 0;
+
+
+    }
+}
