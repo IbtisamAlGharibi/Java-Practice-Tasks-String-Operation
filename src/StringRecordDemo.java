12cc05cd82b7019709fdbfb23807eb2a6367a85d
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-15 15:49:21 +0400

    the string is declared and prited with trim method

---
+++ src/StringRecordDemo.java
@@ -0,0 +1,13 @@
+public class StringRecordDemo {
+    public static void main(String[] args){
+
+        String str = " Ibtisam ";
+        System.out.println(str);
+        System.out.println(str.trim());
+
+
+
+
+
+    }
+}
