df0d475b83b74eb6cf67bb1ab66dd1cb41fc5d88
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-15 16:35:53 +0400

    2 strings are declared with the same text and printed along with thier lengths

---
+++ src/StringNestedLoopDemo.java
@@ -0,0 +1,15 @@
+public class StringNestedLoopDemo {
+
+    public static void main(String[] args){
+        String str = "Ibtisam";
+        String str2 = "Ibtisam";
+
+        System.out.println(str);
+        System.out.println(str2);
+        System.out.println(str.length());
+        System.out.println(str2.length());
+
+
+
+    }
+}
