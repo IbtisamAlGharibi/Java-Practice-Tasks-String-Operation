9f9d373751a09e7b91e5c65a3a09a6a9ed742a9b
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-15 14:31:29 +0400

    string str declared as a sentence and printed on the screen

---
+++ src/StringModifyDemo.java
@@ -0,0 +1,11 @@
+public class StringModifyDemo {
+
+    public static void main(String[] args){
+        String str = " I am solving my tasks regularly  ";
+        System.out.println(str);
+
+
+    }
+
+
+}
