fd348a23c16ea5fb8927e70dab6572bbafc42f65
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-15 14:48:16 +0400

    find the middle char in the string and show in the screen

--- src/StringPositionDemo.java
+++ src/StringPositionDemo.java
@@ -2,7 +2,13 @@ public class StringPositionDemo {
     public static void main(String[] args){
         String str = "Ibtisam";
         System.out.println(str);
-        
+
+        int mid = str.indexOf(3);
+        System.out.println(mid);
+
+
+
+
 
     }
 }
