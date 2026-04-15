aed564c3a3871d1d09f2cc5e5ac638dbd9c258c7
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-15 14:56:05 +0400

    the string is entered as a record with extra space and printed on screen

---
+++ src/StringProcessDemo.java
@@ -0,0 +1,13 @@
+import java.awt.*;
+
+public class StringProcessDemo {
+
+    public static void main(String[] args){
+
+        String str = "001,Ibtisam,It  ";
+        System.out.println(str);
+
+
+
+    }
+}
