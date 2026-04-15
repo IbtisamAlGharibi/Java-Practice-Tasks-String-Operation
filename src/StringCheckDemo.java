7f83854716267f200a8a2e9caa3c3740ce28d127
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-15 15:10:11 +0400

    the string is declared as a message and print part of it using substring method

---
+++ src/StringCheckDemo.java
@@ -0,0 +1,12 @@
+public class StringCheckDemo {
+    public static void main(String[] args){
+        String str = " I am solving my tasks regularly  ";
+        System.out.println(str);
+
+        System.out.println(str.substring(2,6));
+
+
+
+    }
+
+}
