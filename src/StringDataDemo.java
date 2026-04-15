f5773e0f4a487740e304b2a9a567ce308616980a
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-15 15:25:56 +0400

    the string is declared as a sentence and printed in upper and lower cases

---
+++ src/StringDataDemo.java
@@ -0,0 +1,12 @@
+public class StringDataDemo {
+    public static void main ( String[] args){
+
+        String str = " I am solving my tasks regularly  ";
+        System.out.println(str);
+
+        System.out.println(str.toUpperCase());
+        System.out.println(str.toLowerCase());
+
+
+    }
+}
