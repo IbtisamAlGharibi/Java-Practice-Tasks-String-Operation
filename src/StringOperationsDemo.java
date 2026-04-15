2b69461601ee6ba6d63a86c869cd1e842a430eb3
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-15 14:12:52 +0400

    customer name string declared, printed and show the length

---
+++ src/StringOperationsDemo.java
@@ -0,0 +1,11 @@
+public class StringOperationsDemo {
+    public static void main(String[] args){
+
+        String customerName = "Ibtisam";
+        System.out.println(customerName);
+
+        System.out.println(customerName.length());
+
+
+    }
+}
