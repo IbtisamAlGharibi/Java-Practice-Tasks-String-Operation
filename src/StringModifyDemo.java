161370abba7c5633e6a4bbdbd001e10ac9a38029
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-15 14:36:08 +0400

    the string is trimed and check if it contains my word

--- src/StringModifyDemo.java
+++ src/StringModifyDemo.java
@@ -4,6 +4,15 @@ public class StringModifyDemo {
         String str = " I am solving my tasks regularly  ";
         System.out.println(str);
 
+        String updated = str.trim();
+        System.out.println(updated);
+
+        boolean check = str.contains("my");
+        System.out.println(check);
+
+
+
+
 
     }
 
