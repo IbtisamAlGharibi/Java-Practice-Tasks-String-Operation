4f15f84e78b16e83003f23047fc448a5deda215b
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-15 16:31:22 +0400

    Task 13: Use a Do-While Loop to Process a Java String

--- src/StringDoWhileDemo.java
+++ src/StringDoWhileDemo.java
@@ -6,5 +6,28 @@ public class StringDoWhileDemo {
         System.out.println(str);
 
         System.out.println(str.length());
+
+        int i = 0;
+        int lower = 0;
+        int space = 0;
+
+        do {
+            char ch = str.charAt(i);
+            System.out.println(ch);
+
+            if (Character.isLowerCase(ch)) {
+                lower++;
+            }
+
+            if (ch == ' ') {
+                space++;
+            }
+
+            i++;
+
+        } while (i < str.length());
+
+        System.out.println(lower);
+        System.out.println( space);
     }
 }
