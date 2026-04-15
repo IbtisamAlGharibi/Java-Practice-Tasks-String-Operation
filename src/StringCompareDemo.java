445001320f5f52fc55a5284fe41286d11787bee9
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-15 14:22:48 +0400

    str string declared and printed

---
+++ src/StringCompareDemo.java
@@ -0,0 +1,10 @@
+public class StringCompareDemo {
+    public static void main(String[] args){
+
+        String str = "sample";
+        System.out.println(str);
+
+        System.out.println(str.charAt(2));
+
+    }
+}
