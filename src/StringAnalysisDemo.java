fe679edfdc38ed870a11c48c2f08734402904cbe
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-15 15:18:03 +0400

    the string is declared and printed along with its length

---
+++ src/StringAnalysisDemo.java
@@ -0,0 +1,11 @@
+public class StringAnalysisDemo {
+    public static void main(String[] args){
+
+        String str = "Ibtisam";
+        System.out.println(str);
+
+        System.out.println(str.length());
+
+
+    }
+}
