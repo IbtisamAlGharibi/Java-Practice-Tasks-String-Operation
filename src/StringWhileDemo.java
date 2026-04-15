1eb93fcf257a7e780a1dd7a2d4ab101fb9cf459c
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-15 16:24:53 +0400

    Task 12: Use a While Loop to Process a Java String

--- src/StringWhileDemo.java
+++ src/StringWhileDemo.java
@@ -1,3 +1,5 @@
+import java.util.Locale;
+
 public class StringWhileDemo {
     public static void main(String[] args){
         String str = " I am solving my tasks regularly  ";
@@ -5,13 +7,24 @@ public class StringWhileDemo {
 
         System.out.println(str.length());
         int count = 0;
+        int upper=0;
+        int space = 0;
 
         while (count < str.length()){
 
             System.out.println(str.charAt(count));
             if (str.charAt(count) == ' ' ){
+                space =+ 1;
+            }
+            if (Character.isUpperCase(count)){
+                upper =+1;
 
             }
+            count++;
         }
+
+        System.out.println(upper);
+        System.out.println(space);
+        System.out.println(count);
     }
 }
