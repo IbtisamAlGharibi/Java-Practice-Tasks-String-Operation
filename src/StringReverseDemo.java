34eb35ef506181afff6df4b3fab847a87d297e42
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-16 08:21:01 +0400

    the string is declared and printed using for loop

---
+++ src/StringReverseDemo.java
@@ -0,0 +1,15 @@
+public class StringReverseDemo {
+
+    public static void main(String[] args){
+        String str = " I am solving my tasks regularly  ";
+        System.out.println(str);
+
+        System.out.println(str.length());
+
+        int count = 0;
+        for (int i=0; i<str.length();i++){
+            System.out.println(str.charAt(i));
+        }
+
+    }
+}
