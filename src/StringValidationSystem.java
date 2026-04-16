ee685307b6bcf097ca53a04cf4e9b5c8d7ce69cb
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-16 09:30:06 +0400

    printing the string length, upper case and lower case

--- src/StringValidationSystem.java
+++ src/StringValidationSystem.java
@@ -6,4 +6,8 @@ public class StringValidationSystem {
         Scanner scan = new Scanner(System.in);
         String text = scan.next();
 
-}}
+        System.out.println(text.length());
+        System.out.println(text.toUpperCase());
+        System.out.println(text.toLowerCase());
+
+    }}
