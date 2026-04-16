2edb911d1e8aab202f35a5e6ce192b7c1c3685ca
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-16 09:47:31 +0400

    the user is asked to enter a word to check if its in the text

--- src/StringSearchModifySystem.java
+++ src/StringSearchModifySystem.java
@@ -6,6 +6,10 @@ public class StringSearchModifySystem {
         Scanner scan = new Scanner(System.in);
         String text = scan.next();
 
+        System.out.println("Please enter a word you want to check if its in string");
+        Scanner scan3 = new Scanner(System.in);
+        String word = scan3.next();
+        System.out.println(text.contains(word));
 
 
     }}
