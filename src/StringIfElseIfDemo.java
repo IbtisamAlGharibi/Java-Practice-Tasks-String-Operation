9653e8e3cb4cc481f6d219d625fcf17bff4e556f
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-16 08:57:37 +0400

    Task 17: Use If-Else-If Statements with Java String Operations

--- src/StringIfElseIfDemo.java
+++ src/StringIfElseIfDemo.java
@@ -4,4 +4,26 @@ public class StringIfElseIfDemo {
         String name = "Ibtisam";
         System.out.println(name);
 
+        String name2 = "ibtisam";
+
+        if (name == " ") {
+            System.out.println("the string is empty");
+        } else if ( name.length() < 5) {
+            System.out.println("String is less than 5");
+
+        } else if (name.startsWith("I")) {
+            System.out.println("string starts with I");
+        } else if (name.endsWith("n")) {
+            System.out.println("string ends with n");
+        }else if (name.equalsIgnoreCase(name2)){
+            System.out.println("Both names are equals");
+
+        }else {
+            System.out.println("none of the conditions met");
+        }
+
+
+
+
+
     }}
