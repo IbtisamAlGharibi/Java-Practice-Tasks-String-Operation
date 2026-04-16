0e727b2cde9e7a0b9cbc268d419acbee861a230e
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-16 09:46:51 +0400

    ask the user to enter a string using scanner method

---
+++ src/StringSearchModifySystem.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class StringSearchModifySystem {
+    public static void main(String[] args){
+        System.out.println("Please enter a string");
+        Scanner scan = new Scanner(System.in);
+        String text = scan.next();
+
+
+
+    }}
