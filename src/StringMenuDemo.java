ddf97e125fd9e3fbae74b7bfca7c920e3df03eb0
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-16 09:10:11 +0400

    user asked to enter a string using scanner

---
+++ src/StringMenuDemo.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class StringMenuDemo {
+    public static void main(String[] args){
+        System.out.println("Please enter a string");
+        Scanner scan = new Scanner(System.in);
+        scan.next();
+
+
+
+    }
+}
