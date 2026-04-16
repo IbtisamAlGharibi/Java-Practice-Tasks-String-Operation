0d2d142670fde68df831cd30b1398970c6becf86
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-16 08:32:18 +0400

    the string is printed in reverse and coint how many times letter l appears in

--- src/StringReverseDemo.java
+++ src/StringReverseDemo.java
@@ -7,8 +7,20 @@ public class StringReverseDemo {
         System.out.println(str.length());
 
         int count = 0;
+
         for (int i=0; i<str.length();i++){
             System.out.println(str.charAt(i));
+        }
+
+        for (int i = str.length()- 1; i >= 0; i--){
+            System.out.println(str.charAt(i));
+        }
+
+        for (int i=0; i<str.length();i++){
+            if (str.charAt(i) == 'l'){
+                count=+1;
+
+            }
         }
 
     }
