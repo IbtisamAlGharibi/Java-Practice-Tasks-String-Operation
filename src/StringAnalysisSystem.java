77dc700444a86c69e55a3be0e150ee70c3a015a0
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-16 09:41:54 +0400

    the user is asked to enter a word to check if its in the text

--- src/StringAnalysisSystem.java
+++ src/StringAnalysisSystem.java
@@ -9,5 +9,10 @@ public class StringAnalysisSystem {
 
         System.out.println(text.length());
 
+        System.out.println("Please enter a word you want to check if its in string");
+        Scanner scan3 = new Scanner(System.in);
+        String word = scan3.next();
+        System.out.println(text.contains(word));
+
     }}
 
