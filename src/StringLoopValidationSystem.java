1ecbc1e69a31e46b7ebb1b8d6b6100a67e85cc3b
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-16 10:09:40 +0400

    ask the user to enter a string using scanner method

---
+++ src/StringLoopValidationSystem.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class StringLoopValidationSystem {
+    public static void main(String[] args){
+        System.out.println("Please enter a string");
+        Scanner scan = new Scanner(System.in);
+        String text = scan.next();
+
+        System.out.println(text.length());
+
+}}
