17c23bb590f9ab5191c527c40af7d2879cd1c399
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-16 10:03:41 +0400

    the user is asked to enter a string to compare it with the text

--- src/StringCompareValidateSystem.java
+++ src/StringCompareValidateSystem.java
@@ -7,5 +7,11 @@ public class StringCompareValidateSystem {
         Scanner scan = new Scanner(System.in);
         String text = scan.next();
 
+        System.out.println(text.length());
+        System.out.println("Please enter a string you want to compare with");
+        Scanner scan4 = new Scanner(System.in);
+        String str = scan4.next();
+        System.out.println(text.equals(str));
+
     }}
 
