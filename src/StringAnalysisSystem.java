e4038b05f174a9d674c1ac01d60427eb10baa95e
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-04-16 09:41:11 +0400

    the user is asked to enter a string and the length of the string printed on the screen

---
+++ src/StringAnalysisSystem.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class StringAnalysisSystem {
+
+    public static void main(String[] args){
+        System.out.println("Please enter a string");
+        Scanner scan = new Scanner(System.in);
+        String text = scan.next();
+
+        System.out.println(text.length());
+
+    }}
+
